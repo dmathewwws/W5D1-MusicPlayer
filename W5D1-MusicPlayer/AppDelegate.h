87ce310b3c5cdaa9339c43f4eabbb3c5c7d23b95
dannylmathews <dannyl.mathews@gmail.com>
2015-02-12 06:50:28 -0800

    Initial Commit

---
+++ W5D1-MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W5D1-MusicPlayer
+//
+//  Created by Daniel Mathews on 2015-02-12.
+//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W5D1-MusicPlayer/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  W5D1-MusicPlayer
+//
+//  Created by Daniel Mathews on 2015-02-12.
+//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
